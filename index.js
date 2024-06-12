60ab75b06154b3e6b433e68c0a9b06a471a9258b
hasanali207 <hasanali142228@gmail.com>
2024-06-12 11:53:18 +0600

    Added Payment Method SSLCOMMRECE

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const SSLCommerzPayment = require('sslcommerz-lts')
 var jwt = require('jsonwebtoken');
 require("dotenv").config();
 
@@ -14,6 +15,11 @@ app.use(express.json());
 
 
 app.use(cors());
+
+
+const store_id = process.env.STORE_ID
+const store_passwd = process.env.STORE_PASS
+const is_live = false 
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -369,6 +375,64 @@ app.patch('/review/updateItem/:id', async(req, res) => {
     res.send(result)
 });
 
+
+
+app.post('/payment', async(req,res) =>{
+  const item = req.body
+  const tran_id = new ObjectId().toString()
+  const data = {
+    total_amount: item.totalFee,
+    currency: 'BDT',
+    tran_id: tran_id, // use unique tran_id for each api call
+    success_url: `http://localhost:5000/ApplyScholar/${item._id}`,
+    fail_url: 'http://localhost:3030/fail',
+    cancel_url: 'http://localhost:3030/cancel',
+    ipn_url: 'http://localhost:3030/ipn',
+    shipping_method: 'Courier',
+    product_name: 'Computer.',
+    product_category: 'Electronic',
+    product_profile: 'general',
+    cus_name: 'Customer Name',
+    cus_email: 'customer@example.com',
+    cus_add1: 'Dhaka',
+    cus_add2: 'Dhaka',
+    cus_city: 'Dhaka',
+    cus_state: 'Dhaka',
+    cus_postcode: '1000',
+    cus_country: 'Bangladesh',
+    cus_phone: '01711111111',
+    cus_fax: '01711111111',
+    ship_name: 'Customer Name',
+    ship_add1: 'Dhaka',
+    ship_add2: 'Dhaka',
+    ship_city: 'Dhaka',
+    ship_state: 'Dhaka',
+    ship_postcode: 1000,
+    ship_country: 'Bangladesh',
+};
+
+
+const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
+sslcz.init(data).then(apiResponse => {
+    // Redirect the user to payment gateway
+    let GatewayPageURL = apiResponse.GatewayPageURL
+    res.send({url:GatewayPageURL})
+    console.log('Redirecting to: ', GatewayPageURL)
+});
+
+app.post('/ApplyScholar/:id', async(req, res)=>{
+    const id = req.params.id
+    console.log(id)
+    res.redirect(`http://localhost:5173/ApplyScholar/${id}`)
+})
+
+
+
+
+
+
+
+})
 
 
     // await client.db("admin").command({ ping: 1 });
